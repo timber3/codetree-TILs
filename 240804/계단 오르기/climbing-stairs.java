0019a2b3e593c99d9f8695c2fa335aab8b714295
오승현 <75405129+timber3@users.noreply.github.com>
2024-08-04 22:23:13 +0900

    Title: 계단 오르기, Time: 101ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240804/계단 오르기/climbing-stairs.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    static int[] arr;
+    static int n;
+
+    public static void main(String[] args) throws Exception {
+        n = Integer.parseInt(br.readLine());
+        
+        arr = new int[n+1];
+        if (n >= 2)
+            arr[2] = 1;
+        if (n >= 3)
+            arr[3] = 1;
+
+        for (int i = 5 ; i <= n ; i ++) {
+            arr[i] = arr[i-2] + arr[i-3];
+        }
+
+        System.out.println(arr[n]);
+    }
+}
