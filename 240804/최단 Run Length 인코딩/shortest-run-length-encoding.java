545d23f560fe6beb931e4e7596c8957e160da1d1
오승현 <75405129+timber3@users.noreply.github.com>
2024-08-04 21:54:39 +0900

    Title: 최단 Run Length 인코딩, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240804/최단 Run Length 인코딩/shortest-run-length-encoding.java
+++ 240804/최단 Run Length 인코딩/shortest-run-length-encoding.java
@@ -6,43 +6,59 @@ public class Main {
     static StringTokenizer st;
     static StringBuilder sb = new StringBuilder();
 
+    static char[] arr;
+    static int result = Integer.MAX_VALUE;
+
     public static void main(String[] args) throws Exception {
         String str = br.readLine();
         int size = str.length();
-        boolean same = true;
 
-        for (int i = 0 ; i < size-1 ; i ++) {
-            if (str.charAt(i) != str.charAt(i+1)) {
-                same = false;
-                break;
-            }
+        arr = new char[size];
+
+        for (int i = 0 ; i < size ; i ++) {
+            arr[i] = str.charAt(i);
         }
 
-        if (!same) {
-            while(str.charAt(0) != str.charAt(size-1)) {
-                str = str.charAt(size-1) + str.substring(0, size-1);
+        for (int i = 0; i < size-1 ; i ++) {
+            char temp = arr[size-1];
+            
+            for (int j = size-1 ; j > 0 ; j --) {
+                arr[j] = arr[j-1];
             }
-        }
+            arr[0] = temp;
 
-        int count = 1;
-        for (int i = 0 ; i < size-1 ; i ++) {
-            if (str.charAt(i) == str.charAt(i+1)) {
-                count ++;
-            } else {
-                sb.append(str.charAt(i));
+            // for (int j = 0 ; j <= size -1 ; j ++) {
+            //     System.out.print(arr[j]);
+            // }
+            // System.out.println();
+
+            int count = 1;
+            for (int j = 0 ; j < size - 1 ; j ++) {
+                if (arr[j] == arr[j+1]) {
+                    count ++;
+                } else {
+                    sb.append(arr[j]);
+                    sb.append(count);
+                    count = 1;
+                }
+            }
+            if (count > 1) {
+                sb.append(arr[0]);
                 sb.append(count);
-                count = 1;
             }
-        }
-        if (same) {
-            sb.append(str.charAt(0));
-            sb.append(count);
-        }
+            if (arr[size-1] != arr[size-2]) {
+                sb.append(arr[size-1]);
+                sb.append(1);
+            }
 
-        if (str.charAt(size-1) != str.charAt(size-2)) {
-            sb.append(str.charAt(size-1) + "" + 1);
+            // System.out.println(sb);
+
+            result = Math.min(result, sb.toString().length());
+
+            sb = new StringBuilder();
         }
         
-        System.out.println(sb.toString().length());
+        System.out.println(result);
+
     }
 }
