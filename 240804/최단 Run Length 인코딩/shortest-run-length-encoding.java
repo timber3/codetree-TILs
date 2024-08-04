4f5f3bb0e62d97636c987238affcfc195444a8d3
오승현 <75405129+timber3@users.noreply.github.com>
2024-08-04 17:14:50 +0900

    Title: 최단 Run Length 인코딩, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240804/최단 Run Length 인코딩/shortest-run-length-encoding.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+
+        String str = br.readLine();
+
+        int size = str.length();
+
+        while(str.charAt(0) == str.charAt(size-1)) {
+            str = str.charAt(size-1) + str.substring(0, size-1);
+        }
+
+        int count = 1;
+        for (int i = 0 ; i < size-1 ; i ++) {
+            if (str.charAt(i) == str.charAt(i+1)) {
+                count ++;
+            } else {
+                sb.append(str.charAt(i) + "" + count);
+                count = 1;
+            }
+        }
+
+        if (str.charAt(size-1) != str.charAt(size-2)) {
+            sb.append(str.charAt(size-1) + "" + 1);
+        }
+        
+        System.out.println(sb.toString().length());
+    }
+}
