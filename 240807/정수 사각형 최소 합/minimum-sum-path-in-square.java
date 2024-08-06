fc88088012155fb2a866b70c41fd8507aeb2596d
오승현 <75405129+timber3@users.noreply.github.com>
2024-08-07 01:10:58 +0900

    Title: 정수 사각형 최소 합, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 240807/정수 사각형 최소 합/minimum-sum-path-in-square.java
+++ 240807/정수 사각형 최소 합/minimum-sum-path-in-square.java
@@ -24,16 +24,18 @@ public class Main {
             }
         }
 
-        for (int i = 0 ; i <= n ; i ++) {
-            dp[0][i] = 1000001;
-            dp[i][n] = 1000001;
+        dp[1][n-1] = map[1][n-1];
+
+        for (int i = n-2 ; i >= 0 ; i --) {
+            dp[1][i] = dp[1][i+1] + map[1][i];
         }
 
-        dp[0][n-1] = 0;
-        dp[1][n] = 0;
+        for (int i = 2 ; i <= n ; i ++) {
+            dp[i][n-1] = dp[i-1][n-1] + map[i][n-1];
+        }
 
-        for (int i = 1 ; i <= n ; i++) {
-            for (int j = n-1 ; j >= 0 ; j--) {
+        for (int i = 2 ; i <= n ; i++) {
+            for (int j = n-2 ; j >= 0 ; j--) {
                 dp[i][j] = Math.min(dp[i-1][j] + map[i][j], dp[i][j+1] + map[i][j]);
             }
         }
